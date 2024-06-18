7711cd309a507a343ca20718dd2334fbb89a0aab
andrew-Linsanity <andrew.preferredandy@gmail.com>
2024-06-18 19:22:50 +0100

    Fix the nested structure of the function: addSingleTravelerToGroup

--- src/main/java/com/LightSplit/demo/Controller/groupController.java
+++ src/main/java/com/LightSplit/demo/Controller/groupController.java
@@ -107,25 +107,23 @@ public class groupController {
     public ResponseEntity<?> addSingleTravelerToGroup(@PathVariable String groupId, @PathVariable String travelerId) {
         Optional<Group> groupOptional = groupRepo.findById(groupId);
         Optional<Traveler> travelerOptional = travRepo.findById(travelerId);
-        if(groupOptional.isPresent()) {
-            if(travelerOptional.isPresent()) {
-                HashSet<String> set = new HashSet<>();
-                String newName = travelerOptional.get().getNickName();
-                // check if traveler's name already exist in the group
-                Group group = groupOptional.get();
-                for(Traveler trav : group.getTravelers()) {
-                    set.add(trav.getNickName());
-                }
-                if(set.contains(newName)) return new ResponseEntity<String>(newName + " already exists in the Group.", HttpStatus.CONFLICT);
-                group.getTravelers().add(travelerOptional.get());
-                groupRepo.save(group);
-                return new ResponseEntity<Group>(group, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<String>("Traveler with ID: " + travelerId + "is not found.", HttpStatus.NOT_FOUND);
-            }
-        } else {
+        if(!groupOptional.isPresent()) {
             return new ResponseEntity<String>("Group with ID: " + groupId + "is not found.", HttpStatus.NOT_FOUND);
         }
+        if(!travelerOptional.isPresent()) {
+            return new ResponseEntity<String>("Traveler with ID: " + travelerId + "is not found.", HttpStatus.NOT_FOUND);
+        }
+        HashSet<String> set = new HashSet<>();
+        String newName = travelerOptional.get().getNickName();
+        // check if there are duplicates of the new traveler's name
+        Group group = groupOptional.get();
+        for(Traveler trav : group.getTravelers()) {
+            set.add(trav.getNickName());
+        }
+        if(set.contains(newName)) return new ResponseEntity<String>(newName + " already exists in the Group.", HttpStatus.CONFLICT);
+        group.getTravelers().add(travelerOptional.get());
+        groupRepo.save(group);
+        return new ResponseEntity<Group>(group, HttpStatus.OK);
     }
 
     // DeleteMapping: remove traveler from a group
