5190f19323df1d0bea3e54d0b81d2e4075de04ab
andrew-Linsanity <andrew.preferredandy@gmail.com>
2024-09-20 15:56:40 -0400

    Stop tracking SecurityConstants.java

--- src/main/java/com/LightSplit/demo/Security/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.LightSplit.demo.Security;
-
-public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
-    public static final String JWT_SECRET = "Lin1jdwowekkrdmn00errenn310deewrdfgderwrfsdfq143423430s3eseirjwi3jdnfshu3dh3848445erer3444nndfe132232d2934e";
-}
